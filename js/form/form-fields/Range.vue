7fc70b7332ea8bf54dee41159b564c326a3a7caf
basslhm <contact@kompo.io>
2022-02-20 15:09:22 -0300

    Add input range element

---
+++ js/form/form-fields/Range.vue
@@ -0,0 +1,30 @@
+<template>
+    <vl-form-field v-bind="$_wrapperAttributes">
+        <input
+            type="range"
+            v-model="component.value"
+            class="vlFormControl"
+            v-bind="$_attributes"
+            v-on="$_events"
+        />
+        <output class="vlInputWrapper"><div class="vlFormControl" v-html="$_value || 0"/></output>
+    </vl-form-field>
+</template>
+
+<script>
+import Field from '../mixins/Field'
+import HasInputAttributes from '../mixins/HasInputAttributes'
+
+export default {
+    mixins: [Field, HasInputAttributes],
+    computed: {
+        $_attributes() {
+            return {
+                ...this.$_defaultFieldAttributes,
+                ...this.$_defaultInputAttributes
+            }
+        },
+    },
+}
+</script>
+
